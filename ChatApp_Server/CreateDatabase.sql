9b9aa6e5dd00eae6a58e0b837d2a92fd174116a4
anhdungbmt2001 <anhdungbmt2001@gmail.com>
2022-12-27 18:22:17 +0700

    Added CreateDatabase script

---
+++ ChatApp_Server/CreateDatabase.sql
@@ -0,0 +1,34 @@
+﻿USE [master];
+GO
+
+CREATE DATABASE [CS511_ChatApp];
+GO
+
+USE [CS511_ChatApp];
+GO
+
+CREATE TABLE Clients(
+    Username varchar NOT NULL PRIMARY KEY,
+    Pass varchar,
+    DisplayName nvarchar
+);
+
+CREATE TABLE Groups(
+    ID int NOT NULL PRIMARY KEY,
+    DisplayName nvarchar
+);
+
+CREATE TABLE GroupClientLinks(
+    ClientUsername varchar NOT NULL FOREIGN KEY REFERENCES Clients(Username),
+    GroupID int NOT NULL FOREIGN KEY REFERENCES Groups(ID)
+);
+
+CREATE TABLE Messages(
+    ID int NOT NULL PRIMARY KEY,
+    Content ntext NOT NULL,
+    GroupID int NOT NULL FOREIGN KEY REFERENCES Groups(ID),
+    ClientUsername varchar NOT NULL FOREIGN KEY REFERENCES Clients(Username),
+    TimeSent datetime NOT NULL
+);
+
+GO
